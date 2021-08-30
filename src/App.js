b57ec2bf5fe0a7e58adeb59f18029535caa9f0e1
ayush dubey <ayushdubeydps@gmail.com>
2021-08-30 20:25:33 +0530

    rep2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Board from "./components/Board";
+
 function App() {
-  return <div className="App">My App</div>;
+  return (
+    <div className="App">
+      <h1>TIC TAC TOE!</h1>
+      <Board />
+    </div>
+  );
 }
 
 export default App;
